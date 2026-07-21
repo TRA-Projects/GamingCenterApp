10d55b86070e28e9f17f5d6827adbce0b3914036
“Sharouq <sharouqalbadi12@gmail.com>
2026-07-21 14:46:17 +0400

    Update booking model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
@@ -23,7 +23,40 @@ namespace GammingCenter.Models
         [Required(ErrorMessage = "Status is required")]
         [StringLength(30)]
         public string Status { get; set; }
+        //==============================================
+        // Gaming Device
+        [Required]
+        [ForeignKey("GamingDevice")]
+        public int GamingDeviceId { get; set; }
 
-       
+        public virtual GamingDevice GamingDevice { get; set; } // navigation property
+
+
+        // Visitor
+        //every booking assigned to one visitor
+        [Required]
+        [ForeignKey("Visitor")]
+        public int VisitorId { get; set; }
+
+        public virtual Visitor Visitor { get; set; } // navigation property
+
+
+        // Booking Type
+        //every booking classifies to one booking type
+        [Required]
+        [ForeignKey("BookingType")]
+        public int BookingTypeId { get; set; }
+
+        public virtual BookingType BookingType { get; set; }// navigation property
+
+
+        // Available Slot
+        //every booking has one available slot
+        [Required]
+        [ForeignKey("AvailableSlot")]
+        public int AvailableSlotId { get; set; }
+
+        public virtual AvailableSlot AvailableSlot { get; set; } // navigation property
+
     }
 }
