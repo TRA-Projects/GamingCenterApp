a213d637b1d6abcaf0e8ab97fd70767903d86a0d
alzahraalsinani <alzahra.sinani@gmail.com>
2026-07-21 12:55:00 +0400

    add project structure

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Visitor.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Visitor.cs
@@ -1,6 +1,35 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace GammingCenter.Models
 {
     public class Visitor
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VisitorId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string VisitorName { get; set; } // user input
+
+        [Required]
+        [MaxLength(9)]
+        public string PhoneNumber { get; set; } // user input
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; } // user input
+
+        [Required]
+        [MaxLength(3)]
+        public int Age { get; set; } // user input
+
+        [Required]
+        [MaxLength(10)]
+        public string Gender { get; set; } // user input
+
+
     }
 }
