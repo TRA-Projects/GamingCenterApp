96bfe4255723589832405c0596ed8ad0ce0d2024
alzahraalsinani <alzahra.sinani@gmail.com>
2026-07-21 13:15:19 +0400

    add visitor property

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Category.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Category.cs
@@ -1,6 +1,22 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
     public class Category
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string CategoryName { get; set; } // user input
+
+        [Required]
+        public string Description { get; set; }
+
+
     }
 }
