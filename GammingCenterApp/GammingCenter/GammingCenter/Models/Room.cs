711a9037e0f5c9065d08c6572ab7e7c110314ef6
“manar <albreikimanar@gmail.com>
2026-07-21 11:20:45 +0400

    adding Room model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Room.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Room.cs
@@ -1,6 +1,28 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
+    [Table("Room")]
     public class Room
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string RoomName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoomType { get; set; }
+
+        [Range(1, 1000)]
+        public int Capacity { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string RoomStatus { get; set; }
     }
 }
