f0855796e94e53f9416bbd0f2336f9e1401fd477
malak aloufi <mkmalaufi@gmail.com>
2026-07-21 13:17:23 +0400

    adding payment Model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Payment.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Payment.cs
@@ -1,6 +1,44 @@
-﻿namespace GammingCenter.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
     public class Payment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //system generated
+        public int paymentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0, double.MaxValue)]
+        public decimal amount { get; set; }               // calculated from booking
+
+        [Required]
+        [MaxLength(50)]
+        public string paymentMethod { get; set; }          // from list -"Cash"| "Credit Card" | "Debit Card"
+
+        [Required]
+        [MaxLength(30)]
+        public string paymentStatus { get; set; } = "Pending"; // default value - "Pending" | "Paid" | " Failed"
+
+
+
+
+        // foreign key - every payment belongs to exactly one booking 
+
+        [Required]                 // from list - selected booking 
+        [ForeignKey("Booking")]
+        public int bookingId { get; set; }
+
+        public virtual Booking Booking { get; set; }      // navigation property 
+
+
+
+
+
+
+
     }
 }
