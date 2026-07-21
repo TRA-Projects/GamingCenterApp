9113dc2abc5101b2658bb21617f200d04d7c08c9
alzahraalsinani <alzahra.sinani@gmail.com>
2026-07-21 13:58:49 +0400

    add Review property

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Category.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Category.cs
@@ -1,6 +1,19 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
     public class Category
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryId { get; set; }// system generated 
+
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } // user input
+
+        public string description { get; set; } // user input 
     }
 }
--- GammingCenterApp/GammingCenter/GammingCenter/Models/Review.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Review.cs
@@ -1,6 +1,37 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
     public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reviewId { get; set; } // system generated
+
+
+        [Required]
+        [Range(1, 5)]
+        public int rating { get; set; } // user input - from 1 to 5
+
+
+        public string comment { get; set; } // user input
+
+
+        [Required]
+        public DateTime reviewDate { get; set; } = DateTime.Now; //system generated — set to DateTime.Now
+
+
+        // foreign key - links to Visitor ID
+        [Required] 
+        public int visitorId { get; set; } // from list -selected visitor
+
+
+        // foreign key - links to GamingDevice ID
+        [Required] 
+        public int deviceId { get; set; } // from list - selected device
+
+
+
     }
 }
