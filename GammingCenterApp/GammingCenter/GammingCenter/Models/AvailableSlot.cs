28018fc964d4cd74557e7b43b70853244c39cf1d
“manar <albreikimanar@gmail.com>
2026-07-21 11:43:40 +0400

    add AvailableSlots model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/AvailableSlot.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/AvailableSlot.cs
@@ -1,6 +1,27 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
+    [Table("AvailableSlots")]
     public class AvailableSlot
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SlotId { get; set; }
+
+
+        [Required(ErrorMessage = "Duration is required")]
+        [Range(1, 24, ErrorMessage = "Duration must be between 1 and 24 hours")]
+        public int Duration { get; set; }
+
+
+        [Required(ErrorMessage = "Availability status is required")]
+        public bool IsAvailable { get; set; }
+
+
+        [Required(ErrorMessage = "Slot date is required")]
+        [DataType(DataType.Date)]
+        public DateTime SlotDate { get; set; }
     }
 }
