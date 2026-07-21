30d82735b12f1ab8251f5884ce772074e87159ee
“Sharouq <sharouqalbadi12@gmail.com>
2026-07-21 11:47:06 +0400

    Update booking model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
@@ -8,19 +8,19 @@ namespace GammingCenter.Models
         [Key]
         public int BookingId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Booking Date is required")]
         public DateTime BookingDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Player Number is required")]
         [Range(1, 20)]
         public int PlayerNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Total price is required")]
         [Column(TypeName = "decimal(10,2)")]
         [Range(0.01, double.MaxValue)]
         public decimal TotalPrice { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Status is required")]
         [StringLength(30)]
         public string Status { get; set; }
 
