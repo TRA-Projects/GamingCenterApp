e80c9f22acae368f3d4efd240cd74f4db4d8434a
“manar <albreikimanar@gmail.com>
2026-07-21 11:47:34 +0400

    edit in Competition model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Competition.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Competition.cs
@@ -11,29 +11,35 @@ namespace GammingCenter.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CompetitionId { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public string CompetitionName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Competition name is required")]
+        [MaxLength(100, ErrorMessage = "Competition name cannot exceed 100 characters")]
+        public string CompetitionName { get; set; } 
+
+
+        [Required(ErrorMessage = "Start date is required")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "End date is required")]
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "Players number is required")]
         [Range(2, 100, ErrorMessage = "Players number must be between 2 and 100")]
         public int PlayersNo { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string CompetitionStatus { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        public string DevicesName { get; set; }
+        [Required(ErrorMessage = "Competition status is required")]
+        [MaxLength(50, ErrorMessage = "Competition status cannot exceed 50 characters")]
+        public string CompetitionStatus { get; set; } 
+
+
+        [Required(ErrorMessage = "Devices name is required")]
+        [MaxLength(100, ErrorMessage = "Devices name cannot exceed 100 characters")]
+        public string DevicesName { get; set; } 
 
     }
 }
