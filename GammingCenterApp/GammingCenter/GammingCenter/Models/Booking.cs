2ccf87b13af318d79d625fd197e30a8ef793d5e4
“Sharouq <sharouqalbadi12@gmail.com>
2026-07-21 11:41:20 +0400

    Add Booking Model

--- GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Models/Booking.cs
@@ -1,6 +1,29 @@
-﻿namespace GammingCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GammingCenter.Models
 {
     public class Booking
     {
+        [Key]
+        public int BookingId { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int PlayerNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0.01, double.MaxValue)]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Status { get; set; }
+
+       
     }
 }
