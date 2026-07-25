364e5e501e21c116646aeb48af3233e6786fbbdb
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-07-25 21:28:26 +0400

    feat: add gaming device repository with CRUD and status management

--- GammingCenterApp/GammingCenter/GammingCenter/Repositories/GamingDeviceRepository.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Repositories/GamingDeviceRepository.cs
@@ -1,6 +1,88 @@
-﻿namespace GammingCenter.Repositories
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GammingCenter.Repositories
 {
     public class GamingDeviceRepository
     {
+        //All Repo to access Db context
+        private readonly GammingCenterContext Context;
+
+        public GamingDeviceRepository(GammingCenterContext _Context)
+        {
+            Context = _Context;
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+        // 1-Add Device Method
+
+        public void AddGamingDevice(GamingDevice device)
+        {
+
+            Context.GamingDevices.Add(device);
+            Context.SaveChanges();
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+        // 2-update Device Method
+
+        public void UpdateGamingDevice(GamingDevice device)
+        {
+            Context.GamingDevices.Update(device);
+            Context.SaveChanges();
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+        // 3-Delete Device Method
+
+        public void DeleteGamingDevice(GamingDevice device)
+        {
+            Context.GamingDevices.Remove(device);
+            Context.SaveChanges();
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+        // 4-Search Device Method
+        public GamingDevice SearchGamingDevice(int DeviceId)
+        {
+            return
+                Context.GamingDevices.FirstOrDefault(d => d.DeviceID == DeviceId);
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+
+        // 5-View Available Device Method
+        public List<GamingDevice> GetAvailableDevice()
+        {
+            return
+               Context.GamingDevices.Where(d => d.Status == "Available").ToList();
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+
+        // 6-change Device Status Method
+        public void changeDeviceStatus(int DeviceId, string Status)
+        {
+
+            var device = Context.GamingDevices.FirstOrDefault(d => d.DeviceID == DeviceId);
+
+            if (device != null)
+            {
+                device.Status = Status;
+                Context.SaveChanges();
+            }
+        }
     }
 }
