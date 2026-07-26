414f37fa4004d62fab07ac01e89aadc9f53f0200
malak aloufi <mkmalaufi@gmail.com>
2026-07-26 13:18:02 +0400

    add paymentRepo

--- GammingCenterApp/GammingCenter/GammingCenter/Repositories/PaymentRepository.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Repositories/PaymentRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace GammingCenter.Repositories
+﻿using GammingCenter.Models;
+
+namespace GammingCenter.Repositories
 {
     public class PaymentRepository
     {
+       
+        private GammingCenterContext context;
+
+        
+        public PaymentRepository(GammingCenterContext _context)
+        {
+            context = _context;
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            return context.Payments.ToList();
+        }
+
+        
+        public Payment GetPaymentById(int id)
+        {
+            return context.Payments.FirstOrDefault(p => p.paymentId == id);
+        }
+
+        
+        public void Add(Payment payment)
+        {
+            context.Payments.Add(payment);
+            context.SaveChanges();
+        }
+
+       
+        public void Update()
+        {
+            context.SaveChanges();
+        }
+
+       
+        public void Delete(Payment payment)
+        {
+            context.Payments.Remove(payment);
+            context.SaveChanges();
+        }
     }
 }
