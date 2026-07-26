baf046ba463d1127fcbeb1bc2015af0a09ae9860
“manar <albreikimanar@gmail.com>
2026-07-26 15:06:24 +0400

    add (Update Competition) function

--- GammingCenterApp/GammingCenter/GammingCenter/Services/CompetitionService.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Services/CompetitionService.cs
@@ -1,6 +1,7 @@
 ﻿using GammingCenter.DTOs.CompetitionDTO;
 using GammingCenter.Models;
 using GammingCenter.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GammingCenter.Services
 {
@@ -15,35 +16,47 @@ namespace GammingCenter.Services
             repo = _repo;
         }
 
-        // Create Competition
-        public int CreateCompetition(CompetitionDTO dto)
+        // ==== Create Competition =====
+        public int CreateCompetition(CompetitionInputDTO dto)
         {
 
             Competition c = new Competition();
-
-
             c.CompetitionName = dto.CompetitionName;
-
             c.StartDate = dto.StartDate;
-
             c.EndDate = dto.EndDate;
-
             c.PlayersNo = dto.PlayersNo;
-
             c.DevicesName = dto.DevicesName;
-
             c.RoomId = dto.RoomId;
-
-
             // Default Status
             c.CompetitionStatus = "Upcoming";
 
-
             repo.CreateCompetition(c);
-
 
             return c.CompetitionId;
         }
+        // ==== Update Competition ====
+        public bool UpdateCompetition(int id, CompetitionInputDTO dto)
+        {
+            Competition competition = repo.GetCompetitionById(id);
 
-    }  
+
+            if (competition == null)
+                return false;
+
+            competition.CompetitionName = dto.CompetitionName;
+            competition.StartDate = dto.StartDate;
+            competition.EndDate = dto.EndDate;
+            competition.PlayersNo = dto.PlayersNo;
+            competition.DevicesName = dto.DevicesName;
+            competition.RoomId = dto.RoomId;
+
+            repo.UpdateCompetition();
+
+            return true;
+        }
+
+
+
+
+    }
 }
