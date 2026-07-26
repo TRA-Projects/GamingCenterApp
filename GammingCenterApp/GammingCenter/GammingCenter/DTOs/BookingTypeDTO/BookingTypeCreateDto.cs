d6857a86a0ca42fe30509633d6fc5e344263a911
“Anoud <anoudalsaidi1993@gmail.com>
2026-07-26 15:39:09 +0400

    Add Booking Type DTOs

---
+++ GammingCenterApp/GammingCenter/GammingCenter/DTOs/BookingTypeDTO/BookingTypeCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GammingCenter.DTOs.BookingType
+{
+    public class BookingTypeCreateDto
+    {
+        [Required(ErrorMessage = "Booking type name is required")]
+        [MaxLength(50, ErrorMessage = "Booking type name cannot exceed 50 characters")]
+        public string TypeName { get; set; }
+
+        [MaxLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
+        public string Description { get; set; }
+    }
+}
---
+++ GammingCenterApp/GammingCenter/GammingCenter/DTOs/BookingTypeDTO/BookingTypeResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GammingCenter.DTOs.BookingType
+{
+    public class BookingTypeResponseDto
+    {
+        public int BookingTypeID { get; set; }
+
+        public string TypeName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
