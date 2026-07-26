9558eee858c5fca6fe1cbf3ca64ca9d1529ab811
“Sharouq <sharouqalbadi12@gmail.com>
2026-07-26 10:13:14 +0400

    update on BookingServise + add BookingController+ add BookingDTO

--- GammingCenterApp/GammingCenter/GammingCenter/Controllers/BookingController.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Controllers/BookingController.cs
@@ -1,6 +1,40 @@
-﻿namespace GammingCenter.Controllers
+﻿using GammingCenter.DTOs.BookingDTO;
+using GammingCenter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GammingCenter.Controllers
 {
-    public class BookingController
+    public class BookingController : Controller
     {
+        //create booking
+        // Service object used to perform booking operations
+        private BookingService bookingService;
+
+        public BookingController(BookingService bookingService)
+        {
+            // Dependency Injection
+            this.bookingService = bookingService;
+        }
+
+        // Display Create Booking page
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Save a new booking
+        [HttpPost]
+        public IActionResult Create(BookingDTO dto)
+        {
+            if (ModelState.IsValid)
+            {
+                bookingService.CreateBooking(dto);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(dto);
+        }
     }
 }
--- GammingCenterApp/GammingCenter/GammingCenter/Services/BookingService.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Services/BookingService.cs
@@ -1,16 +1,43 @@
-﻿using GammingCenter.Models;
+﻿using GammingCenter.DTOs.BookingDTO;
+using GammingCenter.Models;
+using GammingCenter.Repositories;
 
 namespace GammingCenter.Services
 {
     public class BookingService
     {
-        //Add booking service
-        public interface IBookingService
+        //Crate booking
+        // Repository object used to access booking data
+        private BookingRepository bookingRepo;
+
+        public BookingService(BookingRepository bookingRepo)
         {
-            // Create a new booking
-            void CreateBooking(Booking booking);
+            // Dependency Injection
+            this.bookingRepo = bookingRepo;
         }
-        //==========================================
+
+        // Business Logic for creating a booking
+        public void CreateBooking(BookingDTO dto)
+        {
+            Booking booking = new Booking();
+
+            booking.VisitorId = dto.VisitorId;
+            booking.GamingDeviceId = dto.GamingDeviceId;
+            booking.BookingTypeId = dto.BookingTypeId;
+            booking.AvailableSlotId = dto.AvailableSlotId;
+            booking.PlayerNumber = dto.PlayerNumber;
+            booking.TotalPrice = dto.TotalPrice;
+
+            // Set booking date automatically
+            booking.BookingDate = DateTime.Now;
+
+            // Set default booking status
+            booking.Status = "Pending";
+
+            // Save booking using Repository
+            bookingRepo.AddBooking(booking);
+        }
+        //========================================================
 
     }
 }
