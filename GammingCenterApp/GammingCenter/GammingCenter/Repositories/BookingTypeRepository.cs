90d4761e11fac27f50b011701be869fdc95bf808
“Anoud <anoudalsaidi1993@gmail.com>
2026-07-26 15:00:56 +0400

    Allow Repo to access context by inject constractor

--- GammingCenterApp/GammingCenter/GammingCenter/Repositories/BookingTypeRepository.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Repositories/BookingTypeRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace GammingCenter.Repositories
+﻿using GammingCenter.Models;
+
+namespace GammingCenter.Repositories
 {
     public class BookingTypeRepository
     {
+        //Allow Repo to access context 
+        private readonly GammingCenterContext Context;
+
+        //CONSTRACTOR
+        public BookingTypeRepository(GammingCenterContext context)
+        {
+            Context = context;
+        }
+
+        /////////////////////////////////////////////////////////
+        
+        //1-Add Booking Type
+        public void AddBookingType(BookingType bookingType)
+        {
+            Context.BookingTypes.Add(bookingType);
+            Context.SaveChanges();
+        }
+
+
     }
 }
