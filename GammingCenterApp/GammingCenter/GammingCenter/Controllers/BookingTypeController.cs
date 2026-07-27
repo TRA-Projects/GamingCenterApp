148d60b1c9c14fd4d4d899fcb7fc943ed6feead8
“Anoud <anoudalsaidi1993@gmail.com>
2026-07-27 09:45:25 +0400

    Add Booking Type

--- GammingCenterApp/GammingCenter/GammingCenter/Controllers/BookingTypeController.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Controllers/BookingTypeController.cs
@@ -1,6 +1,40 @@
-﻿namespace GammingCenter.Controllers
+﻿using GammingCenter.DTOs.BookingType;
+using GammingCenter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GammingCenter.Controllers
 {
-    public class BookingTypeController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingTypeController:ControllerBase
     {
+        //Allow Controller to Access Service
+        private readonly BookingTypeService _service;
+
+        //constrator
+        public BookingTypeController(BookingTypeService service)
+        {
+            _service = service;
+        }
+
+        /////////////////////////////////////////////////////////
+
+
+        // 1- Add Booking Type
+
+        [HttpPost]
+        public IActionResult AddGamingDevice(BookingTypeCreateDto dto)
+        {
+            _service.AddBookingType(dto);
+
+            return Ok("Booking type added successfully");
+        }
+
+        ////////////////////////////////////////////////////////
+
+
+        // 2- Update Booking Type
+        public IActionResult 
+
     }
 }
