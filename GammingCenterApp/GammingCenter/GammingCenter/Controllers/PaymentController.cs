605c08aa18d180c067702af6a351052e58a34807
malak aloufi <mkmalaufi@gmail.com>
2026-07-27 13:59:58 +0400

    Add Payment Controller

--- GammingCenterApp/GammingCenter/GammingCenter/Controllers/PaymentController.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Controllers/PaymentController.cs
@@ -1,6 +1,80 @@
-﻿namespace GammingCenter.Controllers
+﻿using GammingCenter.DTOs.PaymentDTO;
+using GammingCenter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GammingCenter.Controllers
 {
-    public class PaymentController
+    [ApiController]
+    [Route("payment")]
+    public class PaymentController : ControllerBase
     {
+
+        private PaymentService paymentService;
+
+        public PaymentController(PaymentService _paymentService)
+        {
+            paymentService = _paymentService;
+        }
+
+
+
+        // ======================================= Make Payment ================================= //
+
+        [HttpPost("MakePayment")]
+        public IActionResult MakePayment([FromBody] PaymentInputDTO payment)
+        {
+            int paymentId = paymentService.MakePayment(payment);
+
+            if (paymentId == 0)
+                return NotFound();
+
+            return Ok(new { PaymentId = paymentId });
+        }
+
+
+
+        // =============================== View Payment Details =======================//
+
+        [HttpGet("GetPaymentDetails/{paymentId}")]
+        public IActionResult GetPaymentDetails([FromRoute] int paymentId)
+        {
+            PaymentOutputDTO payment = paymentService.GetPaymentDetails(paymentId);
+
+            if (payment == null)
+                return NotFound();
+
+            return Ok(payment);
+        }
+
+
+
+        // ========================== Update Payment Status ======================//
+
+        [HttpPut("UpdateStatus/{paymentId}")]
+        public IActionResult UpdateStatus([FromRoute] int paymentId, [FromBody] UpdatePaymentStatusDTO dto)
+        {
+            bool updated = paymentService.UpdatePaymentStatus(paymentId, dto);
+
+            if (!updated)
+                return NotFound();
+
+            return Ok("Updated successfully");
+        }
+
+
+
+        // ============================== Print Invoice / Receipt =========================//
+
+        [HttpGet("PrintInvoice/{paymentId}")]
+        public IActionResult PrintInvoice([FromRoute] int paymentId)
+        {
+            PaymentOutputDTO invoice = paymentService.PrintInvoice(paymentId);
+
+            if (invoice == null)
+                return NotFound();
+
+            return Ok(invoice);
+        }
+
     }
 }
