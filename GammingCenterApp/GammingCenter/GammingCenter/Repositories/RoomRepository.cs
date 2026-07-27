cd32f7abbfbe2cd1f4e43a0395d930c43b3d2e28
“Sharouq <sharouqalbadi12@gmail.com>
2026-07-27 09:14:05 +0400

    add room service

--- GammingCenterApp/GammingCenter/GammingCenter/Repositories/RoomRepository.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Repositories/RoomRepository.cs
@@ -1,11 +1,27 @@
-﻿namespace GammingCenter.Repositories
+﻿using GammingCenter.Models;
+
+namespace GammingCenter.Repositories
 {
     public class RoomRepository
     {
-       
-          
+        // Database context used to access the database
+        private GammingCenterContext context;
 
-            
+        public RoomRepository(GammingCenterContext context)
+        {
+            // Dependency Injection
+            this.context = context;
         }
+
+        // Add a new room to the database
+        public void AddRoom(Room room)
+        {
+            context.Rooms.Add(room);
+            context.SaveChanges();
+        }
+
+
+
+    }
     }
 
--- GammingCenterApp/GammingCenter/GammingCenter/Services/RoomService.cs
+++ GammingCenterApp/GammingCenter/GammingCenter/Services/RoomService.cs
@@ -1,6 +1,34 @@
-﻿namespace GammingCenter.Services
+﻿using GammingCenter.DTOs.RoomDTO;
+using GammingCenter.Models;
+using GammingCenter.Repositories;
+
+namespace GammingCenter.Services
 {
     public class RoomService
     {
+
+        // Repository object used to access room data
+        private RoomRepository roomRepo;
+
+        public RoomService(RoomRepository roomRepo)
+        {
+            // Dependency Injection
+            this.roomRepo = roomRepo;
+        }
+
+        // Business Logic for creating a room
+        public void CreateRoom(RoomDTO dto)
+        {
+            Room room = new Room();
+
+            // Transfer data from DTO to Model
+            room.RoomName = dto.RoomName;
+            room.RoomType = dto.RoomType;
+            room.Capacity = dto.Capacity;
+            room.RoomStatus = dto.RoomStatus;
+
+            // Save room using Repository
+            roomRepo.AddRoom(room);
+        }
     }
 }
