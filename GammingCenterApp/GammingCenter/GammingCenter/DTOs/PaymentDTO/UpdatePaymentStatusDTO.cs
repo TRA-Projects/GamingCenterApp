2366e1edc7f9ca051615d643bb0beb01026032b7
malak aloufi <mkmalaufi@gmail.com>
2026-07-27 11:00:42 +0400

    Add UpdatePaymentStatusDTO

---
+++ GammingCenterApp/GammingCenter/GammingCenter/DTOs/PaymentDTO/UpdatePaymentStatusDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GammingCenter.DTOs.PaymentDTO
+{
+    public class UpdatePaymentStatusDTO
+    {
+        [Required(ErrorMessage = "Value should not be null.")]
+        [StringLength(30, ErrorMessage = "Payment status cannot exceed 30 characters.")]
+        public string paymentStatus { get; set; }
+    }
+}
