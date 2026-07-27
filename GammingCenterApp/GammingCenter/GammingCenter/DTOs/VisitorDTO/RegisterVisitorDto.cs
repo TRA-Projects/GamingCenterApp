43eae556f1eb9eaf1ebbae2a0a460e5a2a3a8cdc
alzahraalsinani <alzahra.sinani@gmail.com>
2026-07-27 10:45:12 +0400

    Add RegisterVisitorDto

---
+++ GammingCenterApp/GammingCenter/GammingCenter/DTOs/VisitorDTO/RegisterVisitorDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GammingCenter.DTOs.VisitorDTO
+{
+    public class RegisterVisitorDto
+    {
+        [Required(ErrorMessage = "Visitor name is required")]
+        [MaxLength(100)]
+        public string VisitorName { get; set; }
+
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [MaxLength(9)]
+        public string PhoneNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Age is required")]
+        [MaxLength(3)]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Gender is required")]
+        [MaxLength(10)]
+        public string Gender { get; set; } 
+
+    }
+}
